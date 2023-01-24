c6f6ac0d66c82d77f3a108feb42e7e4307b21a55
meddhiaka <kassab@fsb.u-carthage.tn>
2023-01-24 17:24:43 +0100

    Creating post details

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom"
+import useFetch from "./useFetch"
 
 export default function PostDetails() {
     const {id} = useParams()
+    const {data: post, IsPending, error} = useFetch('http://localhost:1337/posts/' + id)
+
     return (
         <div className="post-details">
-            <h2>Post Details, {id}</h2>
+                { IsPending && <div>Loading ...</div>}
+                { error && <div>{ error }</div>}
+                { post && (
+                    <article>
+                        <h2>{ post.title }</h2>
+                        <p>Written by { post.author }</p>
+                        <div>{ post.body }</div>
+                    </article>
+                )}
         </div>
     )
 }
